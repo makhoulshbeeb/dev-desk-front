54ae9528698ed3db7d3c2072df663f15948c2b5f
makhoul shbeeb <makhoulshbeeb@gmail.com>
2024-07-27 01:00:20 +0300

    created header and hero section

---
+++ dev-desk/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <HeroSection></HeroSection>
+    </div>
+  );
+}
+
+export default App;
---
+++ dev-desk/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import './styles/Header.css';
+import Button from './Button';
+import Input from '../components/Input';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+export default function Header() {
+    return (
+        <div className='header-container flex'>
+            <nav className='flex'>
+                <div className='logo'>
+                    Dev<span>Desk</span>
+                </div>
+
+
+                <a href='!#'>Home</a>
+                <a href='!#'>About</a>
+                <a href='!#'>Favorites</a>
+
+            </nav>
+            <div className="flex auth">
+                <FontAwesomeIcon icon={faSearch} size='xl'></FontAwesomeIcon>
+                <Input name='search' id='search' placeholder='Search . . .'></Input>
+                <Button bgColor='--primary-color' text='Log In' borderRadius='2rem'></Button>
+                <Button bgColor='--background-light' text='Sign Up' borderRadius='2rem'></Button>
+            </div>
+
+        </div>
+    );
+}
